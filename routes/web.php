99c370f42466a7c9e7c6acb7564dbde8b97960d0
Nguyen Minh Sang <nguyenminhsangvlg2005@gmail.com>
2026-03-02 10:30:35 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\clients\HomeController;
+use App\Http\Controllers\clients\AboutController;
+use App\Http\Controllers\clients\ToursController;
+use App\Http\Controllers\clients\TravelGuidesController;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/about', [AboutController::class, 'index'])->name('about');
+Route::get('/tours', [ToursController::class, 'index'])->name('tours');
+Route::get('/travel-guides', [TravelGuidesController::class, 'index'])->name('travel-guides');
